c44ec0acb58d0efb2cad63e547ddda94f2d25b2b
real-tea <akash.0711a@gmail.com>
2023-12-23 11:48:35 +0530

    final chanes

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,35 @@
+
+const Privacypolicy = () => {
+  return (
+    <>
+      <div className="text-lg text-center px-14 md:px-28 xl:px-72 mt-28">
+        <h2 className="text-2xl font-bold">Privacy Policy</h2>
+        <br />
+        <br />
+        <p>
+          The walls is an app powered by the Spotify web API. By choosing to use this app, you
+          agree to the use of your Spotify account username and data for your top
+          artists and tracks.
+        </p>
+        <br />
+        <p>
+          None of the data used by the walls is stored or collected anywhere, and
+          it is NOT shared with any third parties. All information is used solely
+          for displaying your Top Tracks .
+        </p>
+        <br />
+        <p>
+          Although you can rest assured that your data is not being stored or used
+          maliciously, if you would like to revoke the wall&#39;s permissions, you
+          can visit
+          <a href="http://www.spotify.com/account/apps/?_ga=2.57194153.2059435232.1677244602-1044990631.1616788427"> your apps page </a>
+           and click &#39;REMOVE ACCESS&#39; on the walls.
+          <a href="https://support.spotify.com/us/article/spotify-on-other-apps/"> Here </a>
+          is a more detailed guide for doing so.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Privacypolicy
